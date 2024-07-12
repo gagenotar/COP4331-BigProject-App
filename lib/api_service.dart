5549ff885e27f5e1a3ff6fb44be0e8701f19e7d9
Olga Ivashyna <146783667+oivashyna@users.noreply.github.com>
2024-07-12 14:17:11 -0400

    Update api_service.dart
    Update and 'completed' api endpoints

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -4,9 +4,9 @@ import 'package:bson/bson.dart';
 
 
 class ApiService{
-  static const String baseUrl = 'http://localhost:5001/api'; // Replace with your server URL
+  static const String baseUrl = 'https://journey-journal-cop4331-71e6a1fdae61.herokuapp.com/api'; // Replace with your server URL
   //login
-  static Future<Map<String, dynamic>> login(String email, String password) async {
+   Future<Map<String, dynamic>> login(String email, String password) async {
     final url = Uri.parse('$baseUrl/login');
     try {
       final response = await http.post(
@@ -28,7 +28,7 @@ class ApiService{
   }
 
     //register
-    static Future<Map<String, dynamic>> register
+  Future<Map<String, dynamic>> register
     ( String firstName, String lastName, String
     email, String
     login, String
@@ -77,6 +77,128 @@ class ApiService{
       throw Exception('Failed to connect to server: $e');
     }
   }
+  //edit
 
+  static Future<Map<String, dynamic>> editEntry
+      (
+      String entryId,
+      Map<String, dynamic> updates) async {
+    final url = Uri.parse('$baseUrl/editEntry/$entryId');
+    try {
+      final response = await http.put(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(updates),
+      );
 
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else if (response.statusCode == 404) {
+        throw Exception('Invalid credentials');
+      } else {
+        throw Exception('Failed to add post');
+      }
+    } catch (e) {
+      throw Exception('Failed to connect to server: $e');
+    }
+  }
+ //delete
+
+  static Future<Map<String, dynamic>> deleteEntry
+      (
+      String entryId,
+      ) async {
+    final url = Uri.parse('$baseUrl/deleteEntry/$entryId');
+    try {
+      final response = await http.delete(
+        url,
+
+      );
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else if (response.statusCode == 404) {
+        throw Exception('Invalid credentials');
+      } else {
+        throw Exception('Failed to delete post');
+      }
+    } catch (e) {
+      throw Exception('Failed to connect to server: $e');
+    }
+  }
+//searchEntries (change)
+
+  static Future<Map<String, dynamic>> searchEntries
+      (
+      String search,
+      ) async {
+    final url = Uri.parse('$baseUrl/searchEntries');
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(<String,String>{'search':search,}),
+      );
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else if (response.statusCode == 404) {
+        throw Exception('Invalid credentials');
+      } else {
+        throw Exception('Failed to search for post');
+      }
+    } catch (e) {
+      throw Exception('Failed to connect to server: $e');
+    }
+  }
+//searchMyEntries (change)
+
+  static Future<Map<String, dynamic>> searchMyEntries
+      (
+      String search,
+      String userId,) async {
+    final url = Uri.parse('$baseUrl/addEntry');
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(<String, String>{
+          'search': search,
+          'userId': userId,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else if (response.statusCode == 404) {
+        throw Exception('Invalid credentials');
+      } else {
+        throw Exception('Failed to search for post');
+      }
+    } catch (e) {
+      throw Exception('Failed to connect to server: $e');
+    }
+  }
+
+//getEntry
+  static Future<Map<String, dynamic>> getEntry
+      (String
+  entryId) async {
+    final url = Uri.parse('$baseUrl/getEntry$entryId');
+    try {
+      final response = await http.get(
+        url,
+      );
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else if (response.statusCode == 404) {
+        throw Exception('Invalid credentials');
+      } else {
+        throw Exception('Failed to get post');
+      }
+    } catch (e) {
+      throw Exception('Failed to connect to server: $e');
+    }
+  }
 }
