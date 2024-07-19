10684d8c80d4b077bb367f4735d0b991d0d4196f
Olga Ivashyna <146783667+oivashyna@users.noreply.github.com>
2024-07-19 19:26:53 -0400

    Update settings_page.dart
    reverting back to original settings page setup

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,55 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:journey_journal_app/components/post.dart';
+import 'package:journey_journal_app/pages/home_page.dart';
 
-class SettingsPage extends StatefulWidget {
+class SettingsPage extends StatelessWidget {
   final String email;
   final Map<String, dynamic> credentials;
   final String defaultProfileImage = 'assets/images/profile-pic.png'; // Path to default profile image
-  final Function(String) updateEmailCallback;
 
   const SettingsPage({
     Key? key,
     required this.credentials,
     required this.email,
-    required this.updateEmailCallback,
   }) : super(key: key);
 
   @override
-  _SettingsPageState createState() => _SettingsPageState();
-}
-
-class _SettingsPageState extends State<SettingsPage> {
-  late TextEditingController firstNameController;
-  late TextEditingController lastNameController;
-  late TextEditingController emailController;
-
-  @override
-  void initState() {
-    super.initState();
-    // Initialize controllers with current values
-    firstNameController = TextEditingController(text: widget.credentials['firstName']);
-    lastNameController = TextEditingController(text: widget.credentials['lastName']);
-    emailController = TextEditingController(text: widget.email);
-  }
-
-  @override
-  void dispose() {
-    // Dispose controllers when not needed to avoid memory leaks
-    firstNameController.dispose();
-    lastNameController.dispose();
-    emailController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: Container(
+    return Container(
+      child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             // Profile Image section
             Container(
@@ -60,62 +29,24 @@ class _SettingsPageState extends State<SettingsPage> {
                 image: DecorationImage(
                   fit: BoxFit.cover,
                   image: AssetImage(
-                    widget.credentials['profileImage'] ?? widget.defaultProfileImage,
+                    credentials['profileImage'] ?? defaultProfileImage,
                   ),
                 ),
               ),
             ),
             SizedBox(height: 20), // Optional spacing after the image
             Divider(),
-            TextField(
-              controller: firstNameController,
-              decoration: InputDecoration(
-                labelText: 'First Name',
-              ),
-            ),
+            Text('First Name: ${credentials['firstName']}', style: TextStyle(fontSize: 16.0)),
             Divider(),
-            TextField(
-              controller: lastNameController,
-              decoration: InputDecoration(
-                labelText: 'Last Name',
-              ),
-            ),
+            Text('Last Name: ${credentials['lastName']}', style: TextStyle(fontSize: 16.0)),
             Divider(),
-            Text('Username: ${widget.credentials['login']}', style: TextStyle(fontSize: 16.0)),
+            Text('Username: ${credentials['login']}', style: TextStyle(fontSize: 16.0)),
             Divider(),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-              ),
-              onChanged: (newEmail) {
-                // Update the email in the parent widget via callback
-                widget.updateEmailCallback(newEmail);
-              },
-            ),
+            Text('Email:  $email ', style: TextStyle(fontSize: 16.0)),
             Divider(),
             // Additional Text widgets as needed
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Handle saving changes here
-          String newFirstName = firstNameController.text;
-          String newLastName = lastNameController.text;
-          String newEmail = emailController.text;
-
-          // Update credentials with setState or your preferred state management approach
-          setState(() {
-            widget.credentials['firstName'] = newFirstName;
-            widget.credentials['lastName'] = newLastName;
-            // Email is updated via the callback in onChanged of TextField
-          });
-
-          // Optionally, navigate back to previous screen or show success message
-          Navigator.pop(context); // Example: Navigate back after saving changes
-        },
-        child: Icon(Icons.save),
       ),
     );
   }
